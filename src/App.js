ed100135cdc43c76f7ade1d5b36b43b00e6a4f23
Shinn Thant <shinnthant@Shinns-Mac-mini.local>
2025-02-18 01:42:27 +0700

    Deploy to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import TextOnImageEditor from './components/ui/TextOnImage';
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-50">
+    <div className="min-h-screen bg-gray-50" >
       <div className="container mx-auto py-8">
         <TextOnImageEditor />
       </div>
